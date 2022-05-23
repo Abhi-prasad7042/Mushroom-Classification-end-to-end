f7d9da456d86a5dc4e5d5f2ae912fe334d9bf5f2
Abhi Prasad <AbhiPrasad7042@gmail.com>
2022-05-23 13:41:40 +0530

    first commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Mushroom classification, predicting wether the mushroom is edible or not',
+    author='abhi-prasad7042',
+    license='MIT',
+)
